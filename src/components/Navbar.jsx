aea6516d3962c775830c7cd190383063648eb5e2
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-03-10 11:49:07 +0500

    added SearchBar component and constants

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from '../utils/constants';
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Stack 
+      direction="row" 
+      alignItems="center" 
+      p={2} 
+      sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+    >
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
   )
 }
 
