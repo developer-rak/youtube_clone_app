2afa3f8af59befb12e5f13ebc43dae8a1bafe999
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-03-10 19:52:00 +0500

    fetching data from rapidAPI added env process videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import SideBar from "./SideBar";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const Feed = () => {
   return (
@@ -12,6 +13,14 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright: 2024 developer-rak
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,8 +9,12 @@ const SideBar = () => {
     <Stack direction="row" sx={{ overflow: "auto", height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } }}>
       {categories.map((category) => (
          <button className='category-btn' style={{ background: category.name === selectedCategory && '#FC1503', color: 'white' }} key={category.name}>
-            <span>{ category.icon }</span>
-            <span>{ category.name }</span>
+            <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>
+               { category.icon }
+            </span>
+            <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>
+               { category.name }
+            </span>
          </button>
       ))}
     </Stack>
