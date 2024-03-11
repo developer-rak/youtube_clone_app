80ddb02bd498f941a598e559005ecd049bfdd5aa
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-03-11 18:22:03 +0500

    added all data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,9 +6,11 @@ import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const Feed = () => {
   const [ selectedCategory, setSelectedCategory ] = useState('New');
+  const [ videos, setVideos ] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
   }, [selectedCategory]);
   
   return (
@@ -28,7 +30,7 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: '#F31503' }}>Videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
+        <Videos videos={videos}/>
       </Box>
     </Stack>
   )
