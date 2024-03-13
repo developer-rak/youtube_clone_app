a0e48a7eba59d850392aec1b561dfac615a48fb1
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-03-13 13:01:57 +0500

    added video category

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { Checklist } from "@mui/icons-material";
+
+import { Video } from './';
+import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const VideoDetail = () => {
+  const [ videoDetail, setVideoDetail ] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]));
+  }, [id]);
+
   return (
-    <div>
-      VideoDetail
-    </div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
